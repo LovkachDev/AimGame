8a4a528222464e1bf2789b2222cfe5a5b68306d6
Lovkach <89075967+LovkachDev@users.noreply.github.com>
2021-10-25 16:47:46 +0300

    Upd v1.231
    Add discriptions

--- assets/scr.js
+++ assets/scr.js
@@ -37,15 +37,15 @@ function BtnStart()
     document.getElementById('field').style.display = 'flex';
     Timer();
 }
-let arr = [ "vova" , {bg : "linear-gradient(to right, #f00, #ffa500, #ff0, #008000, #00f, #4b0082, #ee82ee)"} ,
-            "журкин" , {bg : "-webkit-linear-gradient(45deg, #fb8a61,#f74d72,#d2089a,#7029c9)"} , 
-            "lovkach" , {bg : "linear-gradient(45deg, #F17C58, #E94584, #24AADB, #27DBB1, #FFDC18, #FF3706)"} ,
+let arr = [ "vova" , {bg : "linear-gradient(to right, #f00, #ffa500, #ff0, #008000, #00f, #4b0082, #ee82ee)"} , { disc : "градиент"} ,
+            "журкин" , {bg : "-webkit-linear-gradient(45deg, #fb8a61,#f74d72,#d2089a,#7029c9)"}  , { disc : "градиент"} , 
+            "lovkach" , {bg : "linear-gradient(45deg, #F17C58, #E94584, #24AADB, #27DBB1, #FFDC18, #FF3706)"} , { disc : "градиент"} ,
             "уник123" , {bg : "url(https://i.ibb.co/N9K63Sd/image.png)"} , { disc : "кота в кепке"} , //кот в кепке
-            "1000" , {bg : "-webkit-linear-gradient(90deg, #ddee44,#ff4420)"} , 
-            "lierow" , {bg : "url(https://i.ibb.co/YTWtpbc/image.png)"} , // лицо мем
-            "slavaukraine" , {bg : "url(https://i.ibb.co/X3td9P4/image.png)"} , //украина
-            "standa1one" , {bg : "url(https://i.ibb.co/bzT6VN9/animation-gif.gif)"},// гиф сига
-            "mambitchh" , {bg : "url(https://i.ibb.co/FKV3g2g/image.png)"}, // зеленый чел
+            "1000" , {bg : "-webkit-linear-gradient(90deg, #ddee44,#ff4420)"} , { disc : "Шарик за 1000"} , 
+            "lierow" , {bg : "url(https://i.ibb.co/YTWtpbc/image.png)"} , { disc : "Мем лицо"}, // лицо мем
+            "slavaukraine" , {bg : "url(https://i.ibb.co/X3td9P4/image.png)"} , { disc : "Шар с украиной"} , //украина
+            "standa1one" , {bg : "url(https://i.ibb.co/bzT6VN9/animation-gif.gif)"} , { disc : "Шар с мем гифкой"},// гиф сига
+            "mambitchh" , {bg : "url(https://i.ibb.co/FKV3g2g/image.png)"} , { disc : "Шар с зеленым челом"}, // зеленый чел
         ];
 let promo = document.getElementById('promo');
 function Promo()
@@ -55,7 +55,7 @@ function Promo()
         index = arr.indexOf(promo.value);
         Add = index + 1;
         ColorValue = arr[Add].bg;
-        promo.value = "Промокод активирован, вы получили:" + arr[Add + 1].disc;
+        promo.value = "Вы получили:" + arr[Add + 1].disc;
         Button = document.getElementById('btn').style.background = ColorValue;
         localStorage.setItem('Player' , Button );
         document.getElementById('promo').style.borderBottom = '2px solid #8eff04';
